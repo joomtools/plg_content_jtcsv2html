aba640f618257425db8100231627d50a9948faf4
Guido De Gobbis <10517822+degobbis@users.noreply.github.com>
2018-06-27 15:51:11 +0200

    WIP Database changes

--- src/plugins/content/jtcsv2html/sql/uninstall.mysql.sql
+++ src/plugins/content/jtcsv2html/sql/uninstall.mysql.sql
@@ -1,5 +1,12 @@
 --
--- Lösche Tabelle `#__jtcsv2html`
+-- Delete Tabelle `#__jtcsv2html`
 --
 
 DROP TABLE IF EXISTS `#__jtcsv2html`;
+
+
+--
+-- Delete Tabelle `#__jtcsv2html_assoc`
+--
+
+DROP TABLE IF EXISTS `#__jtcsv2html_assoc`;
